821356e6516150dfcbe98b8cfd0100361ce9d6a1
hnugroho <hnugroho@indo-soft.com>
2016-05-20 15:59:24 +0700

    android work

---
+++ app/platforms/android/platform_www/cordova_plugins.js
@@ -0,0 +1,25 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-inappbrowser/www/inappbrowser.js",
+        "id": "cordova-plugin-inappbrowser.inappbrowser",
+        "clobbers": [
+            "cordova.InAppBrowser.open",
+            "window.open"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-whitelist/whitelist.js",
+        "id": "cordova-plugin-whitelist.whitelist",
+        "runs": true
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-inappbrowser": "1.4.0",
+    "cordova-plugin-whitelist": "1.2.1",
+    "de.appplant.cordova.plugin.hidden-statusbar-overlay": "1.2.0"
+};
+// BOTTOM OF METADATA
+});
