2b520fb8b92c3de3174634794c3c10b54179db36
Sheldon Kotyk <sheldon@kotyk.com>
2016-06-12 21:38:45 -0700

    windows

---
+++ app/platforms/windows/platform_www/cordova_plugins.js
@@ -0,0 +1,29 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-inappbrowser/www/inappbrowser.js",
+        "id": "cordova-plugin-inappbrowser.inappbrowser",
+        "pluginId": "cordova-plugin-inappbrowser",
+        "clobbers": [
+            "cordova.InAppBrowser.open",
+            "window.open"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-inappbrowser/src/windows/InAppBrowserProxy.js",
+        "id": "cordova-plugin-inappbrowser.InAppBrowserProxy",
+        "pluginId": "cordova-plugin-inappbrowser",
+        "merges": [
+            ""
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-inappbrowser": "1.4.0",
+    "cordova-plugin-whitelist": "1.2.1",
+    "de.appplant.cordova.plugin.hidden-statusbar-overlay": "1.2.0"
+};
+// BOTTOM OF METADATA
+});
